d8e78780bbb29858947b39e0811844c830910470
Jon Taylor <jontay81@gmail.com>
2016-08-10 18:26:54 -0500

    plot3.R progress

--- plot3.R
+++ plot3.R
@@ -11,15 +11,56 @@ plot3 <- function(){
     
     #subsets for baltimore, fips == 24510
     pm25 <-  pm25[pm25$fips == 24510, ]
-
     
-    sum(pm25[pm25$year == 1999, ]$Emissions)#
-    
-    png(filename="plot2.png")
-    
-    ggplot(data=pm25bm, aes(pm25bm$year, fill=type)) + geom_bar() +
+    pollutiontype <- function(pm25){
+        pollution <-  c(sum(pm25[pm25$year == 1999, ]$Emissions),
+                        sum(pm25[pm25$year == 2002, ]$Emissions),
+                        sum(pm25[pm25$year == 2005, ]$Emissions),
+                        sum(pm25[pm25$year == 2008, ]$Emissions))
         
-  
+        return(pollution)
+    }
+
+    point <- pm25[pm25$type == "POINT", ]
+    nonpoint <- pm25[pm25$type == "NONPOINT", ]
+    onroad <- pm25[pm25$type == "ON-ROAD", ]
+    nonroad <- pm25[pm25$type == "NON-ROAD", ]
     
-    #dev.off()
+    point <-  c(sum(point[point$year == 1999, ]$Emissions),
+                sum(point[point$year == 2002, ]$Emissions),
+                sum(point[point$year == 2005, ]$Emissions),
+                sum(point[point$year == 2008, ]$Emissions))
+    
+    nonpoint <-  c(sum(nonpoint[nonpoint$year == 1999, ]$Emissions),
+                sum(nonpoint[nonpoint$year == 2002, ]$Emissions),
+                sum(nonpoint[nonpoint$year == 2005, ]$Emissions),
+                sum(nonpoint[nonpoint$year == 2008, ]$Emissions))
+    
+    onroad  <-  c(sum(onroad[onroad$year == 1999, ]$Emissions),
+                sum(onroad[onroad$year == 2002, ]$Emissions),
+                sum(onroad[onroad$year == 2005, ]$Emissions),
+                sum(onroad[onroad$year == 2008, ]$Emissions))
+    
+    nonroad <-  c(sum(nonroad[nonroad$year == 1999, ]$Emissions),
+                sum(nonroad[nonroad$year == 2002, ]$Emissions),
+                sum(nonroad[nonroad$year == 2005, ]$Emissions),
+                sum(nonroad[nonroad$year == 2008, ]$Emissions))
+  
+    years <- c("1999", "2002", "2005", "2008")
+    
+    data <- data.table("type"="Point","years"=years, "emissions"=point)     
+    data2 <- data.table("type"="Non-point","years"=years, "emissions"=nonpoint) 
+    data3 <- data.table("type"="On Road","years"=years, "emissions"=onroad) 
+    data4 <- data.table("type"="Non-road","years"=years, "emissions"=nonroad) 
+    
+    data <- rbind(data,data2)
+    data <- rbind(data,data3)
+    data <- rbind(data,data4)
+    
+    png(filename="plot3.png")
+    
+    ggplot(data=data, aes(data$year, data$emissions, col=type)) + geom_bar()
+    
+    
+    dev.off()
 }
